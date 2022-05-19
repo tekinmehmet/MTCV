b9b1fa95d8beb1c7252b0e113d6daac3a8bd0372
Mehmet Tekin <tekimmehmet@hotmail.com>
2022-05-20 01:38:06 +0300

    devam

---
+++ MTCV/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MTCV.Models.ENTITY;
+
+namespace MTCV.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        ContextCv db = new ContextCv();
+        public ActionResult Index()
+        {
+            var degerler = db.Abouts.ToList();
+            return View(degerler);
+        }
+        public PartialViewResult Experience()
+        {
+            return PartialView();
+        }
+    }
+}
